05444ab51812798a0b43658dcd079dfca024a2ed
Sky Dotel <sky.dotel@gmail.com>
2022-03-02 18:07:54 +0000

    12th challenge

--- myApp.js
+++ myApp.js
@@ -5,6 +5,20 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/public',
+        express.static(__dirname + "/public"));
+
+app.get('/', function(req, res){
+            res.sendFile(__dirname + "/views/index.html")});
+
+app.post('/name',
+    function(req, res){
+        var firstname = req.body.first;
+        var lastname = req.body.last;
+        res.json({"name": `${firstname} ${lastname}` });
+    }
+);
+
 
 
 
