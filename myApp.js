d75e0dd7851502f43cc571b58e22d0b6cc1d9a09
Sky Dotel <sky.dotel@gmail.com>
2022-03-02 17:48:37 +0000

    11th challenge

--- myApp.js
+++ myApp.js
@@ -1,15 +1,9 @@
-
+const bodyParser= require('body-parser');
 
 var express = require('express');
 var app = express();
 
-app.get('/name',
-    function(req, res){
-        var firstname = req.query.first;
-        var lastname = req.query.last;
-        res.json({"name": `${firstname} ${lastname}` });
-    }
-);
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 
