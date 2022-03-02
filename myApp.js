b6df31dc6f3c771583e274819c99995adf1c4554
Sky Dotel <sky.dotel@gmail.com>
2022-03-02 16:29:30 +0000

    7th challenge

--- myApp.js
+++ myApp.js
@@ -1,20 +1,12 @@
-require('dotenv').config();
+
 
 var express = require('express');
 var app = express();
 
-let obj = {"message": "Hello json"};
-
-app.get('/json', function(req, res) {
-
-    if(process.env.MESSAGE_STYLE ==="uppercase"){
-    obj["message"] = obj["message"].toUpperCase();
-    res.json(obj);
-    }
-    else{
-    res.json(obj);
-    }
-})
+app.use(function(req,res,next){
+    console.log(req.method + " " + req.path + " - " + req.ip)
+    next();
+});
 
 
 
